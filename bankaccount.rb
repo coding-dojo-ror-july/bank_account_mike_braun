e030c21f156b3fad9788eb5b910ec723d4a3ed75
mpbraun2 <mpbraun2@gmail.com>
2017-07-14 13:10:17 +0000

    added testing

--- bankaccount.rb
+++ bankaccount.rb
@@ -10,55 +10,57 @@
 #  A user should not be able to set any attributes from the BankAccount class
 
 class BankAccount
-    attr_accessor :checking, :savings, :account_number
+  attr_reader :account_number, :checking, :saving
 
-    @@bank_accounts = 0
+  @@bank_accounts = 0
 
-    def initialize
-        @account_number = account_number
-        @checking = 0
-        @savings = 0
-        @@bank_accounts +=1
-        @view_all = view_all
-        @interest_rate = 0.01
+  def initialize 
+    @account_number = create_account
+    @checking = 0
+    @saving = 0
+    @@bank_accounts += 1
+    # not accessible since we do not have a reader for it
+    @interest_rate = 0.01
+  end
+
+  def deposit(account, amount)
+    if account.downcase == "checking"
+      @checking += amount
+    else
+      @saving += amount 
     end
-    def checking_account
-        puts @checking
+  end
+
+  def withdrawal(account, amount)
+    if account.downcase == "checking"
+      if @checking - amount < 0
+        raise "Insufficient Funds, you have #{@checking} in this account"
+      else
+        @checking -= amount
+      end
+    else
+      if @saving - amount < 0
+        raise "Insufficient Funds, you have #{@saving} in this account"
+      else
+        @saving -= amount
+      end
     end
-    def savings_account
-        puts @savings
-    end
-    def deposit_money_savings (money)
-        @savings += money
-        puts @savings
-    end
-    def deposit_money_checking (money)
-        @checking += money
-    end
-    def withdraw_money_checking (money)
-        if @checking - money < 0
-            puts "Insufficient Funds!"
-        else    
-            @checking -= money
-        end
-    end
-    def withdraw_money_savings (money)
-        if @savings - money < 0
-            puts "Insufficient Funds!"
-        else    
-            @savings -= money
-        end
-    end
-    def view_all
-        @view_all = @checking + @savings
-        puts @view_all
-    end
-    def account_information
-        "Account Number: #{@account_number}\nInterestRate: #{@interest_rate}\n#{self.total}"
-private
-    def user_account
-        # @account_number = Math.random (#some code here)
-        puts @account_number
+  end
+
+  def total
+    "Checking Balance: #{@checking}\nSaving Balance: #{@saving}\nTotal Balance: #{@checking + @saving}"
+  end
+
+  def account_information
+    "Account Number: #{@account_number}\nInterest Rate: #{@interest_rate}\n#{self.total}" 
+  end
+
+  def self.number_of_accounts
+    @@bank_accounts
+  end
+
+  private
+    def create_account
+      Array.new(10).map { rand(1..9) }.join
     end
 end
-user = BankAccount.new.savings_account()
